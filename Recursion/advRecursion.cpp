7cb6cb499179bd99da76eca021520ad373ce6d1a
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-05-27 02:08:15 +0530

    Solved one more que

--- Recursion/advRecursion.cpp
+++ Recursion/advRecursion.cpp
@@ -6,19 +6,44 @@ using namespace std;
 int tilingProblem(int n){    // logic similar to fibonacci series
     if(n == 0 || n == 1) return 1;
 
-    // Vertical
+    // Vertical tile
     int ans1 = tilingProblem(n-1);  // 2xn-1
 
-    // Horizontal
+    // Horizontal tile
     int ans2 = tilingProblem(n-2);  // 2xn-2
 
     return ans1 + ans2;
+}
+
+// Remove Duplicates in String:
+void removeDuplicates(string str, string ans, int i, bool visited[26]){
+
+    if(i == str.size()){
+        cout << "Ans: " << ans << endl;
+        return;
+    }
+
+    int visitedIdx = (int)(str[i] - 'a');
+
+    if(visited[visitedIdx]){
+        removeDuplicates(str, ans, i+1, visited);
+    } else {
+        visited[visitedIdx] = true;
+        removeDuplicates(str, ans+str[i], i+1, visited);
+    }
 }
 
 int main(){
     // Tiling Problem 
     int n = 5;
     cout << "No. of ways tiles(2 x 1) can be placed on floor(2 x "<< n << "): " << tilingProblem(n) << endl;
+
+    // Remove Duplicates in String:
+    string str = "appnnacollege";
+    string ans = "";
+    bool visited[26] = {false};
+    removeDuplicates(str, ans, 0, visited);
+
 
     return 0;
 }
