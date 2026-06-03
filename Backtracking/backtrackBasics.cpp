69c9823d2fc578086c0f5f32e4961efbae4b054b
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-06-03 22:35:45 +0530

    Solved find subsets que

--- Backtracking/backtrackBasics.cpp
+++ Backtracking/backtrackBasics.cpp
@@ -21,13 +21,32 @@ void changeArr(int arr[], int n, int i){
     arr[i] -= 2;   // Backtrack
 }
 
+// Find Subsets of string:
+void printSubsets(string str, string subset){
+    if(str.size() == 0){
+        cout << subset << "\n";
+        return;
+    }
+
+    char ch = str[0];
+    // str.substr(stIdx, length of str)
+    printSubsets(str.substr(1, str.size()-1), subset+ch);   // include
+    printSubsets(str.substr(1, str.size()-1), subset);    // exclude
+}
+
 
 int main() {
     int arr[5] = {0};
     int n = 5;
 
-    changeArr(arr, n, 0);
-    printArr(arr, n);
+    // changeArr(arr, n, 0);
+    // printArr(arr, n);
+
+
+    // Find Subsets of string:
+    string str = "abc";
+    string subset = "";
+    printSubsets(str, subset);
  
     return 0;
 } 
