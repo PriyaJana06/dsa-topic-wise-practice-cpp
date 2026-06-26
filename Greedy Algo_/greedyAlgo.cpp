3cc85074cef5a043b5b716fb75acae43fd80d1b8
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-06-26 23:10:45 +0530

    Solved Fractional Knapsack problem

--- Greedy Algo_/greedyAlgo.cpp
+++ Greedy Algo_/greedyAlgo.cpp
@@ -11,6 +11,15 @@ void printPairs(vector<pair<int, int>> pairVec){
     cout << "--------Sorted pairs---------\n";
 }
 
+// Custom Logic to sort pair values:
+bool compare(pair<int, int> p1, pair<int, int> p2) {
+    return p1.second < p2.second;  // ascending - end
+    // return p1.second > p2.second;  // descending - end
+
+    // return p1.first < p2.first;   // ascending - start
+    // return p1.first > p2.first;   // descending - start
+}
+
 // Que: Activities Selection:
 int maxActivities(vector<int> start, vector<int> end){
     // sort on end-time basis
@@ -29,27 +38,50 @@ int maxActivities(vector<int> start, vector<int> end){
     return count;
 }
 
-// Custom Logic to sort pair values:
-bool compare(pair<int, int> p1, pair<int, int> p2) {
-    return p1.second < p2.second;  // ascending - end
-    // return p1.second > p2.second;  // descending - end
 
-    // return p1.first < p2.first;   // ascending - start
-    // return p1.first > p2.first;   // descending - start
+// Que: Fractional Knapsack:  TC -> O(NlogN)
+bool fKCompare(pair<double,int> p1, pair<double,int> p2){
+    return p1.first > p2.first;
+}
+
+int fractionalKnapsack(vector<int> val, vector<int> wt, int W){
+    int n = val.size();
+    vector<pair<double,int>> ratio(n, make_pair(0.0, 0));   // pair(ratio, idx) -> (double, int)
+
+    for(int i=0; i<n; i++){
+        double r = val[i]/(double)wt[i];
+        ratio[i] = make_pair(r, i);
+    }
+
+    sort(ratio.begin(), ratio.end(), fKCompare);   //O(nlogn)
+
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        int idx = ratio[i].second;   // wt, val
+        if(wt[idx] <= W){
+            ans += val[idx];
+            W -= wt[idx];
+        } else {
+            ans += ratio[i].first * W;
+            W -= wt[idx];
+            break;
+        }
+    }
+    return ans;
 }
 
 int main(){
-
     // Que: Activities Selection:
     vector<int> start = {1, 3, 0, 5, 8, 5};
     vector<int> end = {2, 4, 6, 7, 9, 9};
-
     cout << "No. of max. Activities: " << maxActivities(start, end) << endl;
 
+
+    // Pairs in C++:
     vector<int> start1 = {0, 1, 3};
     vector<int> end1 = {9, 2, 4};
 
-    // pair<int, int>;  // single pair
+    // pair<int, int> p;  // single pair
     // vector<int> vec(5, 0);
 
     // vector stores multiple pair:
@@ -64,6 +96,13 @@ int main(){
     sort(activity.begin(), activity.end(), compare);
 
     printPairs(activity);
+
+
+    // Que: Fractional Knapsack:
+    vector<int> val = {60, 100, 120};
+    vector<int> wt = {10, 20, 30};
+    int W = 50;
+    cout << "Max. Total Value: " << fractionalKnapsack(val, wt, W);
     
     return 0;
 }
