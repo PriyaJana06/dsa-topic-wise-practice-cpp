591b7cae23db6e84bc74b2ba3b4b35e5628cfa27
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-06-28 16:02:48 +0530

    Solved Min. Abs. Difference pairs

--- Greedy Algo_/greedyAlgo.cpp
+++ Greedy Algo_/greedyAlgo.cpp
@@ -102,7 +102,22 @@ int main(){
     vector<int> val = {60, 100, 120};
     vector<int> wt = {10, 20, 30};
     int W = 50;
-    cout << "Max. Total Value: " << fractionalKnapsack(val, wt, W);
+    cout << "Max. Total Value: " << fractionalKnapsack(val, wt, W) << endl;
+
+    
+    // Que: Find sum of Min. absolute difference of pairs:
+    vector<int> A = {4, 1, 8, 7};
+    vector<int> B = {2, 3, 6, 5};
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    int absDiff = 0;
+    for(int i=0; i<A.size(); i++){
+        absDiff += abs(A[i]-B[i]);
+    }
+
+    cout << "Min. Absolute Diffence: " << absDiff << endl;
     
     return 0;
 }
