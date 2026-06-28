01d72328c1bb6cc8acb21a2f1438dd56f934814b
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-06-28 16:33:10 +0530

    removed pairs stl

--- Greedy Algo_/greedyAlgo.cpp
+++ Greedy Algo_/greedyAlgo.cpp
@@ -3,22 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-// Print pairs:
-void printPairs(vector<pair<int, int>> pairVec){
-    for(int i=0; i<pairVec.size(); i++){
-        cout << "A"<< i << ": " << "(" << pairVec[i].first << ", " << pairVec[i].second << ")" << endl;
-    } 
-    cout << "--------Sorted pairs---------\n";
-}
-
-// Custom Logic to sort pair values:
-bool compare(pair<int, int> p1, pair<int, int> p2) {
-    return p1.second < p2.second;  // ascending - end
-    // return p1.second > p2.second;  // descending - end
-
-    // return p1.first < p2.first;   // ascending - start
-    // return p1.first > p2.first;   // descending - start
-}
 
 // Que: Activities Selection:
 int maxActivities(vector<int> start, vector<int> end){
@@ -77,34 +61,13 @@ int main(){
     cout << "No. of max. Activities: " << maxActivities(start, end) << endl;
 
 
-    // Pairs in C++:
-    vector<int> start1 = {0, 1, 3};
-    vector<int> end1 = {9, 2, 4};
-
-    // pair<int, int> p;  // single pair
-    // vector<int> vec(5, 0);
-
-    // vector stores multiple pair:
-    vector<pair<int, int>> activity(3, make_pair(0,0));  // empty pair
-    activity[0] = make_pair(0, 9);
-    activity[1] = make_pair(1, 2);
-    activity[2] = make_pair(2, 4);
-
-    printPairs(activity);
-
-    // Sorting pairs:
-    sort(activity.begin(), activity.end(), compare);
-
-    printPairs(activity);
-
-
     // Que: Fractional Knapsack:
     vector<int> val = {60, 100, 120};
     vector<int> wt = {10, 20, 30};
     int W = 50;
     cout << "Max. Total Value: " << fractionalKnapsack(val, wt, W) << endl;
 
-    
+
     // Que: Find sum of Min. absolute difference of pairs:
     vector<int> A = {4, 1, 8, 7};
     vector<int> B = {2, 3, 6, 5};
