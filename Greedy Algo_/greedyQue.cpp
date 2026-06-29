f9e2564d33850da2824482fcd9e901484b34362d
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-06-29 23:41:58 +0530

    learnt how to use lambda functions in job sequencing

--- Greedy Algo_/greedyQue.cpp
+++ Greedy Algo_/greedyQue.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 // Que: Max. Length Chain of Pairs: variant of Activity Selection
-bool compare(pair<int, int> p1, pair<int,int> p2){
+bool compare(pair<int, int>& p1, pair<int,int>& p2){
     return p1.second < p2.second;   // ascending order
 }
 
-int maxChainLength(vector<pair<int, int>> pairs){
+int maxChainLength(vector<pair<int, int>>& pairs){
     int n = pairs.size();
 
     sort(pairs.begin(), pairs.end(), compare);
@@ -25,7 +25,7 @@ int maxChainLength(vector<pair<int, int>> pairs){
 }
 
 // Que: Indian Coins:  find no. of coins to make change for given amount -> TC = O(n) ~ O(1)
-int coinsChange(vector<int> coins, int amount){
+int coinsChange(vector<int>& coins, int amount){
     int ans = 0;
     int n = coins.size();
 
@@ -38,6 +38,67 @@ int coinsChange(vector<int> coins, int amount){
         }
     } cout << "\n";
     return ans;
+}
+
+// Que: Job Sequencing Problem:  TC = O(N)
+bool compareJob(pair<int, int> p1, pair<int, int> p2){
+    return p1.second > p2.second;   // descending order
+}
+
+int jobSequencing(vector<pair<int, int>> pairs){
+    sort(pairs.begin(), pairs.end(), compareJob);
+    // first - deadline; second -> profit
+
+    int profit = pairs[0].second;
+    int safeDeadline = 2;
+
+    for(int i=1; i<pairs.size(); i++){
+        if(pairs[i].first >= safeDeadline){  // non-overlapping job
+            profit += pairs[i].second;
+            safeDeadline++;
+        }
+    }
+    return profit;
+}
+
+// Using vectors of Objects:
+// Printing Job Sequence: need to preserve idx no.
+class Job{
+    public:
+    int idx;
+    int deadline;
+    int profit;
+
+    Job(int idx, int deadline, int profit){
+        this->idx = idx;
+        this->deadline = deadline;
+        this->profit = profit;
+    }
+};
+
+void jobSequence(vector<pair<int,int>> pairs){
+    int n = pairs.size();
+    vector<Job> jobs;
+
+    for(int i = 0; i<n; i++){
+        jobs.emplace_back(i, pairs[i].first, pairs[i].second);  // vector of objects
+    }
+
+    sort(jobs.begin(), jobs.end(), [](Job &a, Job &b){
+        return a.profit > b.profit;  // descending order sort on basis of profit
+    });  // capture list-> lamda functions-> single use only
+
+    cout << "Selecting Job " << jobs[0].idx << endl;
+    int profit = jobs[0].profit;
+    int safeDeadline = 2;
+
+    for(int i=1; i<n; i++){
+        if(jobs[i].deadline >= safeDeadline){
+            cout << "Selecting Job " << jobs[i].idx << endl;
+            profit += jobs[i].profit;
+            safeDeadline++;
+        }
+    }   
 }
 
 int main(){
@@ -60,6 +121,20 @@ int main(){
     int amount = 1099;
 
     cout << "Min. coins for change: " << coinsChange(coins, amount) << endl;
+
+
+    // Que: Job Sequencing Problem:
+    int n1 = 4;
+    vector<pair<int,int>> jobs(n1, make_pair(0, 0));
+    jobs[0] = make_pair(4, 20);
+    jobs[1] = make_pair(1, 10);
+    jobs[2] = make_pair(1, 40);
+    jobs[3] = make_pair(1, 30);
+
+    cout << "Max. profit from jobs: " << jobSequencing(jobs) << endl;
+    cout << "Sequence of Jobs:\n";
+    jobSequence(jobs);
+
 
     return 0;
 }
