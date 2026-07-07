1891ca5e33536a4ac1eeb8af0cf769635f9b2a7f
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-07-07 13:19:47 +0530

    Learnt to build tree from preorder sequence

--- Binary Trees/basicsBT.cpp
+++ Binary Trees/basicsBT.cpp
@@ -14,13 +14,26 @@ public:
     }
 };
 
+// Build Tree from Preorder:
+static int idx = -1;
 Node* buildTree(vector<int> nodes){
+    idx++;
+    if(nodes[idx] == -1){
+        return nullptr;
+    }
 
+    Node* currNode = new Node(nodes[idx]);
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
 }
 
 int main() {
     // Preorder sequence:
     vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
+    Node* root = buildTree(nodes);
+    cout << "root = " << root->data << endl;
     return 0;
 }
