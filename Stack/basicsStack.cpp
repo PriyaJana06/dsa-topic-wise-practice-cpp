2bd0c087f7d8871291e39c987a95c8dd86d04622
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-07-16 17:00:27 +0530

    Implemented Stack using Vector

--- Stack/basicsStack.cpp
+++ Stack/basicsStack.cpp
@@ -1,10 +1,51 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-// Implementation of Stack using vector:
+// Implementation of Stack using Vector:
+class Stack{
+    vector<int> vec;
+public:
+    void push(int val){  // O(1)
+        vec.push_back(val);
+    }
 
+    void pop(){    // O(1)
+        if(isEmpty()){
+            cout << "Stack Underflow\n";
+            return;
+        }
+        vec.pop_back();
+    }
+
+    int top(){    // O(1)
+        if(isEmpty()){
+            cout << "Stack Underflow\n";
+            return -1;
+        }
+
+        int lastIdx = vec.size()-1;
+        return vec[lastIdx];
+    }
+
+    bool isEmpty(){   // O(1)
+        return vec.size() == 0;
+    }
+};
 
 int main(){
+    Stack s;
+
+    s.push(3);
+    s.push(2);
+    s.push(1);
+
+    // To print Stack Elements:
+    cout << "Stack Elements: ";
+    while(!s.isEmpty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
 
     return 0;
 }
