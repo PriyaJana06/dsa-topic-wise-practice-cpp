2f86384f0726745271f64fcd5805dabc9eb37ff8
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-09-06 20:01:03 +0530

    solved: Implement Stack using 2 Queues

--- Queue/queueQue.cpp
+++ Queue/queueQue.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<stack>
+#include<queue>
 using namespace std;
 
-// Implement Queue using 2 Stack:
+//Que: Implement Queue using 2 Stack:
 class Queue{  // FIFO 
     stack<int> s1;
     stack<int> s2;
@@ -36,9 +37,41 @@ public:
     }
 };
 
+//Que: Implement Stack using 2 Queues:
+class Stack{
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int data){
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        q1.push(data);
+
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+    }
+
+    void pop(){
+        q1.pop();
+    }
+
+    int top(){
+        return q1.front();
+    }
+
+    bool empty(){
+        return q1.empty();
+    }
+};
+
 int main(){
 
-    // queue using 2 stacks:
+    //Que: queue using 2 stacks:
     Queue q;
 
     q.push(1);
@@ -46,11 +79,25 @@ int main(){
     q.push(3);
     q.push(4);
 
+    cout << "Queue using 2 Stacks: ";
     while(!q.empty()){
         cout << q.front() << " ";
         q.pop();
+    } cout << endl;
+
+    //Que: Stack using 2 Queues:
+    Stack s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    cout << "Stack using 2 Queues: ";
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
     }
 
-   
     return 0;
 }
