8380451e6638764888acdc531804b43e0cb02bb0
PRIYANKA JANA <priyanka.jana2106@gmail.com>
2026-09-15 01:44:38 +0530

    solved: Learnt Subtree of Another Tree Que

--- Binary Trees/part2BT.cpp
+++ Binary Trees/part2BT.cpp
@@ -41,7 +41,7 @@ int heightOfTree(Node* root){
     return currHt;
 }
 
-// Que: Diameter of a Tree: TC = O(N^2)
+// Que: Approach_1: Diameter of a Tree: TC = O(N^2)
 int diam1(Node* root){
     if(root == nullptr) return 0;
 
@@ -52,6 +52,7 @@ int diam1(Node* root){
     return max(currDiam, max(leftDiam, rightDiam));
 }
 
+// Approach_2:
 pair<int, int> diam2(Node* root){   // N nodes * O(1) = O(N)
     if(root == nullptr) return make_pair(0, 0);
 
@@ -66,6 +67,50 @@ pair<int, int> diam2(Node* root){   // N nodes * O(1) = O(N)
     return make_pair(finalDiam, finalHt);
 }
 
+// Que: Subtree of another Tree: TC = O(N * M)
+bool isIdentical(Node* root1, Node* root2){
+    // Base Case
+    if(root1 == nullptr && root2 == nullptr){
+        return true;
+    } else if(root1 == nullptr || root2 == nullptr){
+        return false;
+    }
+
+    if(root1->data != root2->data){
+        return false;
+    }
+
+    return isIdentical(root1->left, root2->left) && 
+            isIdentical(root1->right, root2->right);
+}
+
+bool isSubtree(Node* root, Node* subRoot){
+    // Base Case
+    if(root == nullptr && subRoot == nullptr){
+        return true;
+    }
+    else if(root == nullptr || subRoot == nullptr){
+        return false;
+    }
+
+    if(root->data == subRoot->data){
+        // identical or not
+        if(isIdentical(root, subRoot)){   // Edge Case
+            return true;
+        }
+    }
+
+    bool isLeftSubtree = isSubtree(root->left, subRoot);
+
+    // if not found in leftSubtree, then search in rightSubtree.
+    if(!isLeftSubtree){
+        return isSubtree(root->right, subRoot);
+    }
+
+    return true;
+}
+
+
 int main(){
     // Que: Diameter of a Tree:
     vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -73,6 +118,17 @@ int main(){
 
     cout << "Diameter_1 of Tree: " << diam1(root) << endl;
     cout << "Diameter_2 of Tree: " << diam2(root).first << endl;
+
+    // Que: Subtree of Another Tree:
+    Node* subRoot = new Node(2);
+    subRoot->left = new Node(4);
+    subRoot->right = new Node(5);
+
+    if(isSubtree(root, subRoot)){
+        cout << "subRoot is a subtree of root." << endl;
+    } else {
+        cout << "subRoot is NOT a subtree of root." << endl;
+    }
 
     return 0;
 }
